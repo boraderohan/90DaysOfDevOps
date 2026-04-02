97149627cdaeb44f40df856c2b7d78006cfde9bd
Rohan <rohanborade18@gmail.com>
2026-04-02 19:20:14 +0530

    Added terraform-basics folder

---
+++ 2026/day-61/terraform-basics/main.tf
@@ -0,0 +1,91 @@
+# Providers 
+
+provider "aws" {
+
+	region = "ap-south-1"
+
+}
+
+resource "aws_s3_bucket" "demo-bucket-assignment" {
+  
+	bucket = "borade-demo-bucket"
+
+}
+
+
+
+# Key Value pair
+
+resource "aws_key_pair" "terra-week-key" {
+
+  key_name   = "terra-week-automate-key"
+  public_key = file("terraweek-automate-key.pub")
+}
+
+# VPC Default
+
+resource "aws_default_vpc" "default" {
+}
+
+# Security Group
+
+resource "aws_security_group" "my_security_group" {
+
+  name        = "terra-security-group"
+  vpc_id      = aws_default_vpc.default.id # interpolation
+  description = "this is Inbound and outbound rules for your instance Security group"
+
+}
+
+# Inbound & Outbount port rules
+
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+# EC2 instance
+
+
+resource "aws_instance" "my_instance" {
+
+  ami   = "ami-05d2d839d4f73aafb" # OS AMI ID
+
+  instance_type = "t3.micro" # Instance Type
+
+  key_name = aws_key_pair.terra-week-key.key_name # Key pair
+
+  vpc_security_group_ids = [aws_security_group.my_security_group.id] # VPC & Security Group
+
+  # root storage (EBS)
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "TerraWeek-Modified"
+  }
+}
+
